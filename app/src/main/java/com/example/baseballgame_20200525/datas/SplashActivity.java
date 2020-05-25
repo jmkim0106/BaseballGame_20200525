d50f2f64e073a1248f6192332ff7c6d8a03d164d
weekyeon <weekyeon@gmail.com>
2020-05-25 14:39:08 +0900

    스플래쉬 화면에서 2초뒤에 메인으로 전환

--- app/src/main/java/com/example/baseballgame_20200525/datas/SplashActivity.java
+++ app/src/main/java/com/example/baseballgame_20200525/datas/SplashActivity.java
@@ -2,9 +2,12 @@ package com.example.baseballgame_20200525.datas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.baseballgame_20200525.BaseActivity;
+import com.example.baseballgame_20200525.MainActivity;
 import com.example.baseballgame_20200525.R;
 
 public class SplashActivity extends BaseActivity {
@@ -13,6 +16,8 @@ public class SplashActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        setUpEvents();
+        setValues();
     }
 
     @Override
@@ -22,6 +27,16 @@ public class SplashActivity extends BaseActivity {
 
     @Override
     public void setValues() {
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent myIntent = new Intent(mContext, MainActivity.class);
+                startActivity(myIntent);
+
+                finish();
+            }
+        }, 2000);
 
     }
 }
